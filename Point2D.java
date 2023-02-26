47e8d84556228d1b7dba22ae7614be9d69a92519
01PhiHung01 <125170335+01PhiHung01@users.noreply.github.com>
2023-02-26 18:27:35 +0700

    Add files via upload

---
+++ Point2D.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diemtrongmp2chieu;
+
+public class Point2D {
+    private float x;
+    public float getX() {
+        return this.x;
+    } 
+    private float y;
+    public float getY() {
+        return this.y;
+    }
+public void getPoint2D(float x,float y) {
+    getX();
+    getY();
+}
+
+}
